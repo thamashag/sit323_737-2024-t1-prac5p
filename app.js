2b0c25c5baeddc8a0210eea34cbe916615a16b35
thamashag <thamasha1996@gmail.com>
2024-04-23 09:46:01 +1000

    added docker and healthcheck

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Health check route
+app.get('/health', (req, res) => {
+  // Check if your application dependencies are healthy
+  // You can add custom logic here to check the status of your application's dependencies
+  // For simplicity, we'll just respond with a success status code (200)
+  res.status(200).send('OK');
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
   });
